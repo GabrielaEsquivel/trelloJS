cc24575da082851580fb1214e7ec147b82760852
Andrea <andrea.rueda-ext@dominion-global.com>
2022-05-04 19:58:22 -0500

    Agregando nuevo app js y cambiando el id de la lista en donde se añadira la card

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 require('dotenv').config()
 
-if(!process.env.TOKEN && !process.env.KEY){
-  throw new Error('No hay configuración con Api Key y con Token')
+if (!process.env.TOKEN && !process.env.KEY) {
+    throw new Error('No hay configuración con Api Key y con Token')
 }
+let Trello = require("trello");
+let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+let cardTitle = `Card Nueva ${new Date()}`
+
+trello.addCard(cardTitle, "LaunchX Card Description", "6272ffd32ae6f3091dbe0deb",
+    function (error, trelloCard) {
+        if (error) {
+            console.log('Could not add card:', error);
+        }
+        else {
+            console.log('Added card:', trelloCard);
+        }
+    });
+
+    
